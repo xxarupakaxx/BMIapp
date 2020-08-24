6b6b1fe28991385284d9c6b9793356c1393bd191
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-08-24 10:32:48 +0900

    adviceを作った

--- BMI.js
+++ BMI.js
@@ -12,5 +12,25 @@ class BMI{
 
     return bmi;
 }
+
+getAdvice(){
+    let bmi = this.weight/Math.pow(this.height/100 ,2);
+
+    bmi =Math.round(bmi);
+
+    let idealWeight =22 * Math.pow(this.height/100 ,2);
+    let adviceMessage = "理想体重だぞ　うな重食べたい";
+
+    if(bmi > 22){
+        adviceMessage = "太りすぎ　ジャイアンかよ" + idealWeight + "kgを目指しましょう";
+
+    }
+    else if(bmi < 22){
+        adviceMessage ="やせすぎ　ニンジンさん？" + idealWeight + "kgを目指しましょう";    
+    }
+
+    return adviceMessage;
+
+}
 }
 
