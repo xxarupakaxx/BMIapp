29cc29dd26475d67d9b399422306fc156ab1ca7b
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-08-24 09:49:03 +0900

    BMIのclass作成

--- BMI.js
+++ BMI.js
@@ -1,1 +1,18 @@
 "use strict"
+
+class BMI{
+    constructor(height =0,weight= 0){
+        this.height =height;
+        this.weight = weight;
+    }
+    getBMI(){
+    let bmi = this.weight/Math.pow(this.height/100 ,2);
+    
+    bmi *=10;
+    bmi =Math.round(bmi);
+    bmi /=10;
+
+    return bmi;
+}
+}
+
